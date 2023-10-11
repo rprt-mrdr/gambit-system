30edd02ad79baa2dfc646d94870590110f29ecf2
rprt-mrdr <143330480+rprt-mrdr@users.noreply.github.com>
2023-10-11 22:23:53 +1100

    Update Dice.java

--- gambit-system/src/main/java/Dice.java
+++ gambit-system/src/main/java/Dice.java
@@ -33,10 +33,11 @@ public class Dice {
     public static void analysis(int attribute, int gambit) {
 
         int possibleRolls = attribute * gambit;
-        System.out.println(possibleRolls + " possible rolls");
         int effect;
+        int score;
         int minEffect = (gambit - Math.max(gambit, attribute)) + 1;
         int possibleEffectValues = (gambit - minEffect) + 1;
+        int possibleScoreValues = gambit + attribute - 1;
 
         int[] effectValues = new int[possibleEffectValues];
         for (int i = 0; i < effectValues.length; i++) {
@@ -58,30 +59,108 @@ public class Dice {
             effectPercentage[i] = Float.parseFloat(df.format(((float) effectTally[i] / possibleRolls) * 100));
 
         }
-        
+
         float[] cumulativePercentage = new float[possibleEffectValues];
         float runningTotal = 0;
-        for (int i = 0; i < cumulativePercentage.length; i++){
+        for (int i = 0; i < cumulativePercentage.length; i++) {
             runningTotal += effectPercentage[possibleEffectValues - 1 - i];
             cumulativePercentage[possibleEffectValues - 1 - i] = runningTotal;
         }
+
+        int[] scoreValues = new int[possibleScoreValues];
+        for (int i = 0; i < scoreValues.length; i++) {
+            scoreValues[i] = i + 2;
+        }
+
+        int[] scoreTally = new int[possibleScoreValues];
+        for (int i = 0; i < gambit; i++) {
+
+            for (int j = 0; j < attribute; j++) {
+                score = i + j + 2;
+                scoreTally[score - 2]++;
+            }
+
+        }
+
+        float[] scorePercentage = new float[possibleScoreValues];
+        for (int i = 0; i < scorePercentage.length; i++) {
+            scorePercentage[i] = Float.parseFloat(df.format(((float) scoreTally[i] / possibleRolls) * 100));
+
+        }
+
+        float[] cumulativeScorePercentage = new float[possibleScoreValues];
+        float scoreRunningTotal = 0;
+        for (int i = 0; i < cumulativeScorePercentage.length; i++) {
+            scoreRunningTotal += scorePercentage[i];
+            cumulativeScorePercentage[i] = scoreRunningTotal;
+        }
+
+//        System.out.println("Effect:");
+//        for (int i = 0; i < possibleEffectValues; i++) {
+//            System.out.printf("%-7d", effectValues[i]);
+//        }
+//        System.out.println("\nTally, out of " + possibleRolls);
+//        for (int i = 0; i < possibleEffectValues; i++) {
+//            System.out.printf("%-7d", effectTally[i]);
+//        }
+//        System.out.println("\nPercent chance:");
+//        for (int i = 0; i < possibleEffectValues; i++) {
+//            System.out.printf("%-7.2f", effectPercentage[i]);
+//        }
+//        System.out.println("\nCumulative chance: (chance of rolling this effect or higher)");
+//        for (int i = 0; i < possibleEffectValues; i++) {
+//            System.out.printf("%-7.2f", cumulativePercentage[i]);
+//        }
+//        System.out.println("\nScore:");
+//        for (int i = 0; i < scoreValues.length; i++) {
+//            System.out.printf("%-7d", scoreValues[i]);
+//        }
+//        System.out.println("\nTally:");
+//        for (int i = 0; i < scoreValues.length; i++) {
+//            System.out.printf("%-7d", scoreTally[i]);
+//        }
+//        System.out.println("\nPercent chance:");
+//        for (int i = 0; i < scoreValues.length; i++) {
+//            System.out.printf("%-7.2f", scorePercentage[i]);
+//        }
+//        System.out.println("\nCumulative chance: (chance of rolling this score or lower)");
+//        for (int i = 0; i < scoreValues.length; i++) {
+//            System.out.printf("%-7.2f", cumulativeScorePercentage[i]);
+//        }
+//        System.out.println();
         
-        System.out.print("Effect:\n");
-        for (int i = 0; i < possibleEffectValues; i++) {
-            System.out.printf("%-7d", effectValues[i]);
-        }
-        System.out.println("\nTally, out of " + possibleRolls);
-        for (int i = 0; i < possibleEffectValues; i++) {
-            System.out.printf("%-7d", effectTally[i]);
-        }
-        System.out.println("\nPercent chance:");
-        for (int i = 0; i < possibleEffectValues; i++) {
-            System.out.printf("%-7.2f", effectPercentage[i]);
-        }
-        System.out.println("\nCumulative chance:");
+        System.out.println("\n\nEffect (chance higher or equal)\n");
         for (int i = 0; i < possibleEffectValues; i++){
-            System.out.printf("%-7.2f", cumulativePercentage[i]);
+            System.out.printf("%-3d (%.0f%%)\t", effectValues[effectValues.length - 1 - i], cumulativePercentage[effectValues.length - 1 - i]);
+            for (int j = 0; j < effectTally[effectValues.length - 1 - i]; j++){
+                System.out.print("-");
+            }
+            
+            System.out.println();
         }
+        System.out.println("\n\nScore (chance lower or equal)\n");
+        for (int i = 0; i < possibleScoreValues; i++){
+            System.out.printf("%-3d (%.0f%%)\t", scoreValues[i], cumulativeScorePercentage[i]);
+            
+            for (int j = 0; j < scoreTally[i]; j++){
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        
+        int[][] matrix = new int[possibleScoreValues][possibleEffectValues];
+        for (int i = 0; i < gambit; i++){
+            for (int j = 0; j < attribute; j++){
+                matrix[i + j][effect(gambit, j + 1, i + 1) - minEffect]++;
+            }
+        }
+        
+//        for (int i = 0; i < matrix.length; i++){
+//            System.out.println();
+//            for (int j = 0; j < matrix[i].length; j++)
+//                System.out.printf(" %d", matrix[i][j]);
+//            
+//        }
 
     }
 
